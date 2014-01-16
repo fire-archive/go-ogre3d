3420193f75f3f715c9baacd3db21140fa0eba1f5
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-16 12:51:08 -0800

    Make w,x,y,z public variables.

--- quaternion.go
+++ quaternion.go
@@ -8,14 +8,14 @@ import "C"
 
 type Quaternion struct {
 	cquat C.coiQuaternion
-	w, x, y, z float32
+	W, X, Y, Z float32
 }
 
 func (quat *Quaternion) FromRotationMatrix(mat Matrix3) {
 	quat.cquat = C.quaternion_from_rotation_matrix(C.coiMatrix3(mat.cmat))
-	quat.w = float32(quat.cquat.w)
-	quat.x = float32(quat.cquat.x)
-	quat.y = float32(quat.cquat.y)
-	quat.z = float32(quat.cquat.z)
+	quat.W = float32(quat.cquat.w)
+	quat.X = float32(quat.cquat.x)
+	quat.Y = float32(quat.cquat.y)
+	quat.Z = float32(quat.cquat.z)
 }
 
