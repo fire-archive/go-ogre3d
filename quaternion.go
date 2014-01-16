753b799c8886f75c4a91b9e43546b8d680b215c8
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-16 09:57:42 -0800

    Use existing quaternion rather than create a new one.

--- quaternion.go
+++ quaternion.go
@@ -11,13 +11,11 @@ type Quaternion struct {
 	w, x, y, z float32
 }
 
-func FromRotationMatrix(mat Matrix3) (Quaternion) {
-	var quat Quaternion
+func (quat *Quaternion) FromRotationMatrix(mat Matrix3) {
 	quat.cquat = C.quaternion_from_rotation_matrix(C.coiMatrix3(mat.cmat))
 	quat.w = float32(quat.cquat.w)
 	quat.x = float32(quat.cquat.x)
 	quat.y = float32(quat.cquat.y)
 	quat.z = float32(quat.cquat.z)
-	return quat
 }
 
