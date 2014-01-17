51ca1da2aca875a624ac66a9c056467682fe77d9
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-17 11:43:47 -0800

    More quaternion code.

--- quaternion.go
+++ quaternion.go
@@ -5,17 +5,43 @@ package ogre
  #include "llcoi/ogre_interface.h"
 */
 import "C"
-
 type Quaternion struct {
-	cquat C.coiQuaternion
-	W, X, Y, Z float32
+	cptr C.QuaternionHandle
 }
 
-func (quat *Quaternion) FromRotationMatrix(mat Matrix3) {
-	quat.cquat = C.quaternion_from_rotation_matrix(C.coiMatrix3(mat.cmat))
-	quat.W = float32(quat.cquat.w)
-	quat.X = float32(quat.cquat.x)
-	quat.Y = float32(quat.cquat.y)
-	quat.Z = float32(quat.cquat.z)
+func (quat *Quaternion) FromRotationMatrix(mat Matrix3) Quaternion {
+	var result Quaternion
+	m := C.coiMatrix3(mat.cmat)
+	result.cptr = C.quaternion_from_rotation_matrix(&m)
+	return result	
 }
 
+func (quat *Quaternion) FromValues(w, x, y, z float32) Quaternion {
+	var result Quaternion
+	result.cptr = C.quaternion_from_values(C.coiReal(w), C.coiReal(x), C.coiReal(y), C.coiReal(z))
+	return result	
+}
+
+func (quat *Quaternion) ToRotationMatrix(mat *Matrix3) {
+	C.quaternion_to_rotation_matrix(quat.cptr, &mat.cmat)
+}
+
+func (quat *Quaternion) W() float32 {
+	
+	return float32(C.quaternion_get_w(quat.cptr))
+}
+
+func (quat *Quaternion) X() float32 {
+	
+	return float32(C.quaternion_get_x(quat.cptr))
+}
+
+func (quat *Quaternion) Y() float32 {
+	
+	return float32(C.quaternion_get_y(quat.cptr))
+}
+
+func (quat *Quaternion) Z() float32 {
+	
+	return float32(C.quaternion_get_z(quat.cptr))
+}
