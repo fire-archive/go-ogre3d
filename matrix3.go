d358016843240aa14921493a1f33751a90b33171
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-18 14:08:39 -0800

    Finish ToEulerAnglesYXZ().

--- matrix3.go
+++ matrix3.go
@@ -11,5 +11,13 @@ type Matrix3 struct {
 }
 
 func (mat *Matrix3) FromEulerAnglesYXZ(y, x, z float32) {
-	mat.cmat = C.matrix_3_from_euler_angles_Y_X_Z(C.coiRadian(y), C.coiRadian(x), C.coiRadian(z))
+	mat.cmat = C.matrix_3_from_euler_angles_y_x_z(C.coiRadian(y), C.coiRadian(x), C.coiRadian(z))	
+}
+
+func (mat *Matrix3) ToEulerAnglesYXZ(y, x, z *float32) {
+	var yangle, xangle, zangle C.coiRadian
+	C.matrix_3_to_euler_angles_y_x_z(mat.cmat, &yangle, &xangle, &zangle)
+	*y = float32(yangle)
+	*x = float32(xangle)
+	*z = float32(zangle)
 }
