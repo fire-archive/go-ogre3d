978b8f33b34dbe969fa1e399b62d1b9a86a91be2
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-29 17:36:28 -0800

    Add getter and setters to vector2.

--- vector2.go
+++ vector2.go
@@ -24,3 +24,25 @@ func CreateVector2FromValues(x float32, y float32) (Vector2) {
 	result.cptr = C.vector2_create_from_values(C.coiReal(x), C.coiReal(y))	
 	return result
 }
+
+func (vec *Vector2) MultiplyScalar(real float32) (Vector2) {
+	var result Vector2
+	result.cptr = C.vector2_multiply_scalar(vec.cptr, C.coiReal(real))
+	return result
+}
+
+func (vec *Vector2) X() (float32) {
+	return float32(C.vector2_x(vec.cptr))
+}
+
+func (vec *Vector2) Y() (float32) {
+	return float32(C.vector2_y(vec.cptr))
+}
+
+func (vec *Vector2) SetX(real float32) {
+	C.vector2_set_x(vec.cptr, C.coiReal(real))
+}
+
+func (vec *Vector2) SetY(real float32) {
+	C.vector2_set_y(vec.cptr, C.coiReal(real))
+}
